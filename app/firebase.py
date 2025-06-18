921d4f6b28536b4d52363bc5b5cddb9642ba75ef
Prastab Mukhopadhyay <92989287+Prastabm@users.noreply.github.com>
2025-06-18 23:44:35 +0530

    Update firebase.py

--- app/firebase.py
+++ app/firebase.py
@@ -2,7 +2,7 @@ import os
 import firebase_admin
 from firebase_admin import credentials, firestore, auth as fb_auth
 
-cred = credentials.Certificate(os.getenv("FIREBASE_KEY_PATH", "firebase_key.json"))
+cred = credentials.Certificate(os.getenv("FIREBASE_KEY_PATH", "/etc/secrets/firebase_key.json"))
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
